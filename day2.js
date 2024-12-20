6653886e2713baf3360a6b9cce54d1c06a29cf1c
Ankita <bobdeankita24@gmail.com>
2024-12-20 00:54:21 -0330

    data working properly

---
+++ day2.js
@@ -0,0 +1,20 @@
+function change_img(a){
+    console.log(a)
+    let selectimg=document.querySelector('.largeimg');
+    if (a==1){
+        selectimg.src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+    }
+    else if (a==2){
+        selectimg.src="https://images.pexels.com/photos/135940/pexels-photo-135940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+    }
+     else if (a==3){
+        selectimg.src="https://images.pexels.com/photos/381739/pexels-photo-381739.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+    }
+    else if (a==4){
+        selectimg.src="https://images.pexels.com/photos/1887624/pexels-photo-1887624.jpeg?auto=compress&cs=tinysrgb&w=600";
+    }
+    else if (a==5){
+        selectimg.src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+    
+}
+}
