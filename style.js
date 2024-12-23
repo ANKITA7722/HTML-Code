9defe1c85e9d39a68746f304465fb03cbc37f76c
Ankita <bobdeankita24@gmail.com>
2024-12-22 23:35:10 -0330

    create color program

---
+++ style.js
@@ -0,0 +1,42 @@
+//que:- 2
+// function randomePaswordGenrator(lenghtOfPass){
+// let charSrting = "ABCDEFGHIJKLMNOPabdefghijklmnopqrs@#$%^&*123456789";
+// let pass = "";
+// for( i=0; i<lenghtOfPass ;i++){
+//     let randomeindex = Math.floor(Math.randome() * charSrting.lenght + 1)
+//      pass +=  charSrting[randomeindex];
+// }
+// return pass;
+// }
+// console.log(randomePaswordGenrator);
+
+
+//que 1
+// let date = new Date()
+// console.log(date.toLocaleTimeString());
+// console.log(date.toLocaleDateString());
+// setInterval(()=>{
+// let date = new Date()
+// let timee = date.toLocaleTimeString()
+// let dg = document.getElementById("digital_clock");
+// dg.innerHTML =timee
+// },1000)
+
+//que 4:image change program onmouseenter pr 
+        // function fun(){
+        //     let img = document.getElementById("change_image")
+        //     img.src="IMG-ankita.jpg"
+        // }
+        // function fun(){
+        //         let img = document.getElementById("change_img")
+        //          img.src = "whitehill.webp"
+        //     }
+
+
+ //que 5:
+ let array = "apple banan cherry"
+ let arrayToString = array.split(' ');
+ arrayToString.sort()
+ console.log(arrayToString);
+
+
